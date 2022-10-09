449c015cccfaf6e30eb95b3915f58a26f1f35772
Rajgohel0312 <rajgohel2016@gmail.com>
2022-10-09 15:21:50 +0530

    Question 1
    -Solved Question 1 and took the example name like this

---
+++ Question01.java
@@ -0,0 +1,11 @@
+/**
+ * Question01
+ */
+public class Question01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Raj Gohel");
+
+    }
+}
